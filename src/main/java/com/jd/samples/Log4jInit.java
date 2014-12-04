2a36f11283cceae24bffdb20a236da9e7ad808dd
This4u <this4u@sina.com>
2014-12-04 15:44:15 +0800

    init

--- src/main/java/com/jd/samples/Log4jInit.java
+++ src/main/java/com/jd/samples/Log4jInit.java
@@ -11,11 +11,16 @@ public class Log4jInit extends HttpServlet {
 
 	@Override
 	public void init() throws ServletException {
-//		System.out.println("log4j init start...");
-//		String realPath = this.getServletContext().getRealPath("/");
-//		System.out.println(realPath);
-//		String filePath = getInitParameter("log4j");
-//		PropertyConfigurator.configure(realPath + "/WEB-INF/" + filePath);
-//		System.out.println("log4j init finish...");
+		System.out.println("log4j init start...");
+		// String realPath = this.getServletContext().getRealPath("/");
+		// System.out.println(realPath);
+		// String filePath = getInitParameter("log4j");
+		// PropertyConfigurator.configure(realPath + "/WEB-INF/" + filePath);
+		try {
+			Class.forName("org.apache.log4j.PropertyConfigurator");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		System.out.println("log4j init finish...");
 	}
 }
